d701563eeb72d1283cb736112eca0cf010f5e605
jfaquino <39652504+jfaquino@users.noreply.github.com>
2022-05-09 22:06:18 -0400

    Initial commit

---
+++ lib/models/contact.dart
@@ -0,0 +1,9 @@
+class Contact {
+  final String name;
+  final String lastName;
+  final String phone;
+  final String email;
+  final String address;
+
+  Contact(this.name, this.lastName, this.phone, this.email, this.address);
+}
